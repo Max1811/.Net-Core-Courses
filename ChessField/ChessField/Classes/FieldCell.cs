ee9255c43ef0b47765b307fa1650301eb6f5de85
Max <max.yar.eli@gmail.com>
2019-12-02 15:24:45 +0200

    Chess board 1version

---
+++ ChessField/ChessField/Classes/FieldCell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessFieldDrawProgram.Classes
+{
+    public class FieldCell
+    {
+        protected char CellSymbol { get; set; }
+
+        public FieldCell()
+        {
+            CellSymbol = ' ';
+        }
+        public FieldCell(char symbol)
+        {
+            this.CellSymbol = symbol;
+        }
+    }
+}
---
+++ ChessField/ChessField/Messages/MessagesText.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessFieldDrawProgram.Messages
+{
+    public class MessagesText
+    {
+        public string info = "'help' - for all commands, 'start' - to start program, 'exit' - to exit from program";
+        public string inputHeight = "Input Height: ";
+        public string inputWidth = "Input Width: ";
+        public string inputSymbol = "Input Symbol: ";
+        public string stoppedProgram = "The program has been stopped";
+    }
+}
