3926212d7af09e399441712882380aeab3b30487
Max <max.yar.eli@gmail.com>
2019-12-18 15:44:17 +0200

    +refactored Sequence task

---
+++ SequenceTask/SequenceTask/Program.cs
@@ -0,0 +1,17 @@
+﻿using SequenceTask.SequenceUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UI.Start(args);
+        }
+    }
+}
---
+++ SequenceTask/SequenceTask/SequenceOfNumbers/Sequence.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SequenceTask.SequenceOfNumbers
+{
+    public class Sequence
+    {
+        public IEnumerable<int> NumbersSequence { get;  }
+        public Sequence(IEnumerable<int> sequence)
+        {
+            NumbersSequence = sequence;
+        }
+    }
+}
