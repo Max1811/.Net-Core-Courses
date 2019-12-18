a623c774e953bc71750dd890dc0d9b50cda78438
Max <max.yar.eli@gmail.com>
2019-12-18 14:33:31 +0200

    Sequence task v1

---
+++ SequenceTask/SequenceTask/SequenceOfNumbers/Sequence.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace SequenceTask.SequenceOfNumbers
+{
+    public class Sequence
+    {
+        public IEnumerable<int> NumbersSequence { get;  }
+        public Sequence(IEnumerable<int> sequence)
+        {
+            NumbersSequence = sequence;
+        }
+
+    }
+}
---
+++ SequenceTask/SequenceTask/SequenceOfNumbers/SequenceSizing.cs
@@ -0,0 +1,19 @@
+﻿namespace SequenceTask.SequenceOfNumbers
+{
+    public abstract class SequenceSizing
+    {
+        private protected int From { get; }
+        private protected int To { get; }
+
+        public SequenceSizing()
+        {
+            From = 0;
+            To = 0;
+        }
+        public SequenceSizing(int from, int to)
+        {
+            From = from;
+            To = to;
+        }
+    }
+}
